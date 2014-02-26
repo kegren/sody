062788af0060c16d0635fe6a9676adefbfc2eeb1
Kenny Damgren <kennydamgren@gmail.com>
2014-02-26 21:55:29 +0100

    Add new exception for routes

---
+++ Sody/src/Exception/NoRouteFoundException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sody\Exception;
+
+use Exception;
+
+/**
+ * NoRouteFoundException
+ *
+ * @author Kenny Damgren <kennydamgren@gmail.com>
+ * @package Sody
+ */
+class NoRouteFoundException extends Exception
+{
+
+}
